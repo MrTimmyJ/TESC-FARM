36336dd084c1ac71083629a4cbad8da140a7f8ad
Andersen-Ader <work+gh@andersenader.com>
2024-11-12 14:54:35 -0800

    poop emoji main

--- api/main.go
+++ api/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+      "database/sql"
 	"net/http"
 	"time"
-    "fmt"
 )
+
+// GLOBAL VARIABLES (>^^)>
+var db *sql.DB
+var products []Product
 
 type Product struct {
 	Name     string
 	Quantity int
-    Price    int
+	Price    int
 }
 
 type ProduceData struct {
@@ -20,22 +24,52 @@ type ProduceData struct {
 
 func getProduce(c *gin.Context) {
 	data := ProduceData{}
-    data.Retrieved = time.Now()
+      rows, err := db.QueryRows("SELECT name, quantity FROM products;")
+      if err != nil {
+            panic(err.Error())
+      }
+      for rows.Next() {
+            p := Product{}
+            err := rows.Scan(&p.Name, &p.Quantity)
+            if err != nil {
+                  panic(err.Error())
+            }
+            data.Products = append(data.Products, p)
+      }
+	data.Retrieved = time.Now()
 	c.JSON(http.StatusOK, data)
 }
 
+func heart(c *gin.Context) {
+      c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("❤️ "))
+}
+
+func poop(c *gin.Context) {
+      c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("💩"))
+}
+
 func main() {
-    fmt.Println("❤️ ")
-    // Instantiate the router
-    r := gin.Default()
-    // Define root endpoint
+      products = []Product{
+            Product{Name: "Apple", Quantity: 10},
+            Product{Name: "Peach", Quantity: 5},
+            Product{Name: "Tomato", Quantity: 8},
+            Product{Name: "🍇", Quantity: 12},
+      }
+      var err error
+      db, err = sql.Open("sqlite3", "data.db")
+
+	// Instantiate the router
+      r := gin.Default()
+	// Define root endpoint
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "online",
 		})
 	})
-    // Define produce endpoint
+      r.GET("/heart", heart)
+      r.GET("/poop", poop)
+	// Define produce endpoint
 	r.GET("/produce.json", getProduce)
-    // Run router on port 8080
+	// Run router on port 8080
 	r.Run("127.0.0.1:8080")
 }
