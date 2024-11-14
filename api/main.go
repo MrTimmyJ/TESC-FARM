894efcb8abe36dcc30572999c5e3ed83337ca8ca
Studbug <nskkid@gmail.com>
2024-11-14 14:13:15 -0800

    Further fleshed out api controllers and models to handle product requests

--- api/main.go
+++ api/main.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-      "github.com/gin-gonic/gin"
-	  "github.com/Acstrayer/TESCSE-Ecom/api/models"
-	  "github.com/Acstrayer/TESCSE-Ecom/api/controllers"
-      "gorm.io/driver/sqlite"
-      "gorm.io/gorm"
-      "net/http"
-      "time"
+	"github.com/Acstrayer/TESCSE-Ecom/api/controllers"
+	"github.com/Acstrayer/TESCSE-Ecom/api/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // For Jess; from austin: excuse me?
 func poop(c *gin.Context) {
-      c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("💩"))
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("💩"))
 }
 
 func main() {
 	// ...
 	r := gin.Default()
 	models.ConnectDatabase()
-
+	r.GET("/", poop)
 	r.GET("/poop", poop)
 	r.GET("/products", controllers.FindProducts) //All products
-	r.POST("/products", controllers.CreateProduct)
-	r.GET("/products/:id", controllers.FindProduct)
-	r.PATCH("/products/:id", controllers.UpdateProduct)
-	r.DELETE("/products/:id", controllers.DeleteProduct) 
+	r.POST("/products/new", controllers.CreateProduct)
+	r.GET("/product/:id", controllers.FindProduct)
+	r.PATCH("/product/:id/update", controllers.UpdateProduct)
+	r.DELETE("/product/:id/delete", controllers.DeleteProduct)
 
 	err := r.Run("127.0.0.1:8080")
 	if err != nil {
-			panic("Could not run the database.")
+		panic("Could not run the database.")
 	}
 }
--- api/models/product.go
+++ api/models/product.go
@@ -2,10 +2,24 @@
 
 package models
 
+import (
+	"time"
+)
+
+type ProductRequestData struct {
+	Products  []Product
+	Retrieved time.Time
+}
+
 type Product struct {
-  ID     uint   `json:"id" gorm:"primary_key"`
-  Name string `json:"name"`
-  Type string `json:"type"`
-  Quantity uint `json:"quantity`
-  Price int `json: price` //In pennies
+	ID       uint   `json:"id" gorm:"primary_key"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Quantity uint   `json:"quantity`
+	Price    int    `json: price` //In pennies
+}
+
+type CreateProductInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
 }
