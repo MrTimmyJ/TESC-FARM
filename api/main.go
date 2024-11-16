beef78522259eeb83e0bec90a390a88d6a4376c2
Shawn Bird <absent.bird@gmail.com>
2024-11-16 15:53:20 -0800

    recompiled to allow external connections

--- api/main.go
+++ api/main.go
@@ -30,6 +30,6 @@ func main() {
 	r.GET("/orders", controllers.FindOrders)
 	r.POST("/orders/new", controllers.CreateOrder)
 
-	err := r.Run("127.0.0.1:8080")
+	err := r.Run("0.0.0.0:8080")
 	models.Check(err, "Error starting API")
 }
