b6beaa9c8fbbbb3b8ab517b5556fedd7183d9457
haydene907 <haydenedge2013@hotmail.com>
2024-11-16 16:22:59 -0800

    test

---
+++ api/products.sql
@@ -0,0 +1,18 @@
+INSERT INTO products
+(name, quantity, type)
+VALUES 
+('Basil', 5, 'Bunches'),
+('Beans',10,'Bushes'),
+('Beets',30, 'Roots'),
+('Broccoli',30,NULL),
+('Cabbage',15,NULL),
+('Carrots',50,'Roots'),
+('Celeriac',20,NULL),
+('Cilantro', 50, 'Bunches'),
+('Corn',50,'Sweet'),
+('Cucumber',20,NULL),
+('Dill', 50, 'Bunches'),
+('Kale',30,'Bunches'),
+('Lettuce',30,'Heads'),
+('Onions',50,NULL),
+('Parsley',50,'Bunches');
